98f98ccc8574eae383dba132373d6e0c0e856e4d
kobra-soft <code.softwaredev@gmail.com>
2024-05-15 23:57:52 +0100

    Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,20 @@ import { navLists } from "../constants";
 const Navbar = () => {
   return (
     <header className="w-full py-[0.82rem] flex justify-center items-center">
-      <nav className="flex justify-center bg-green-400">
+      <nav className="flex justify-center">
         <div className="flex justify-center w-full"> 
-          <img src={appleImg} alt="Apple" width={13} height={13} />
-          <div className="flex justify-start max-sm:hidden ml-0 mr-0 bg-red-900">
+
+          <img className="bg-red-400 mx-0" src={appleImg} alt="Apple" width={13} height={13} />
+
+          <div className="flex justify-start max-sm:hidden ml-16 mr-0 bg-red-900">
             {navLists.map((nav, index) => (
               <div
                 key={nav}
-                className={`sm:hidden md:flex text-[11.666px] font-light cursor-pointer text-[#cccccc]
+                className={`sm:hidden md:flex text-[12px] font-light cursor-pointer text-[#cccccc]
                  hover:text-white bg-gray-900
                  ${
                   index < navLists.length - 1
-                    ? "sm:mx-[0.0rem] md:mx-[0.86rem] lg:mx-[1.28rem]"
+                    ? "md:pr-[0.86rem] lg:pr-[2.52rem] "
                     : "pr-[0rem] pl-0"
                 }`}
               >
