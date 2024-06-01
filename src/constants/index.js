b7912ac78f01732322361ff82199d3912dfbefeb
kobra-soft <code.softwaredev@gmail.com>
2024-06-01 19:53:25 +0100

    CONSTANTS - Update for NavBar (MenuItems)

--- src/constants/index.js
+++ src/constants/index.js
@@ -99,10 +99,250 @@ export const footerLinks = [
   "Site Map",
 ];
 
-/*   export const footerLinks = [
-    "Privacy Policy",
-    "Terms of Use",
-    "Sales Policy",
-    "Legal",
-    "Site Map",
-  ]; */
+// 2
+// 
+// Search
+//
+// Quick Links
+//
+// Find a Store
+// Accessories
+// AirPods
+// AirTag
+// Apple Trade In
+
+// 3
+//
+// Your Bag is empty.
+//
+// Sign in to see if vou have anv saved items
+//
+// My Profile
+//
+// Orders
+// Your Saves
+// Account
+// Sign in
+
+// 4
+//
+// Store
+// Mac
+// iPad
+// iPhone
+// Watch
+// AirPods
+// TV & Home
+// Entertainment
+// Accessories
+// Support
+
+// 4a
+// 
+// Shop the Latest
+// Mac
+// iPad
+// iPhone
+// Apple Watch
+// Accessories
+// Quick Links
+// Find a Store
+// Order Status
+// Apple Trade In
+// Financing
+// Shop Special Stores
+// Certified Refurbished
+// Education
+// Business
+
+// 4b
+// 
+// Explore All Mac
+// MacBook Air
+// MacBook Pro
+// iMac
+// Mac mini
+// Mac Studio// 
+// Mac Pro
+// Displays
+// Compare Mac
+// Mac Does That
+// Shop Mac
+// Shop Mac
+// Mac Accessories
+// Apple Trade In
+// Financing
+// More from Mac
+// Mac Support
+// AppleCare+ for Mac
+// macOS Sonoma
+// Apps by Apple
+// Continuity
+// iCloud+
+// Mac for Business
+// Education
+
+// 4c
+// 
+// Explore All iPad
+// iPad Pro
+// iPad Air
+// iPad
+// iPad mini
+// Apple Pencil
+// Keyboards
+// Compare iPad
+// Why iPad
+// Shop iPad
+// Shop iPad
+// iPad Accessories
+// Apple Trade In
+// Financing
+// More from iPad
+// iPad Support
+// AppleCare+ for iPad
+// iPadOS 17
+// Apps by Apple
+// iCloud+
+// Education
+
+// 4d
+// 
+// Explore All iPhone
+// iPhone 15 Pro
+// iPhone 15
+// iPhone 14
+// iPhone 13
+// iPhone SE
+// Compare iPhone
+// Switch from Android
+// Shop iPhone
+// Shop iPhone
+// iPhone Accessories
+// Apple Trade In
+// Financing
+// More from iPhone
+// iPhone Support
+// AppleCare+ for iPhone
+// iOS 17
+// Apps by Apple
+// iPhone Privacy
+// iCloud+
+// Wallet, Pay
+// Siri
+
+// 4e
+// 
+// Explore All Apple Watch
+// Apple Watch Series 9
+// Apple Watch Ultra 2
+// Apple Watch SE
+// Apple Watch Nike
+// Apple Watch Hermès
+// Compare Watch
+// Why Apple Watch
+// Shop Watch
+// Shop Apple Watch
+// Apple Watch Studio
+// Apple Watch Straps
+// Apple Watch Accessories
+// Apple Trade In
+// Financing
+// More from Watch
+// Apple Watch Support
+// AppleCare+
+// watchOS 10
+// Apps by Apple
+// Apple Fitness+
+
+// 4f
+// 
+// Explore All AirPods
+// AirPods Pro (2nd
+// generation)
+// AirPods (2nd generation)
+// AirPods (3rd generation)
+// AirPods Max
+// Compare AirPods
+// Shop AirPods
+// Shop AirPods
+// AirPods Accessories
+// More from AirPods
+// AirPods Support
+// AppleCare+ for Headphones
+// Apple Music
+
+// 4g
+// 
+// Explore TV & Home
+// Apple TV 4K
+// HomePod
+// HomePod mini
+// Shop TV & Home
+// Shop Apple TV 4K
+// Shop HomePod
+// Shop HomePod mini
+// Shop Siri Remote
+// TV & Home Accessories
+// More from TV & Home
+// Apple TV Support
+// HomePod Support
+// AppleCare+
+// Apple TV app
+// Apple TV+
+// Home app
+// Apple Music
+// Siri
+// AirPlay
+
+// 4h
+// 
+// Explore Entertainment
+// Apple One
+// Apple TV+
+// Apple Music
+// Apple Arcade
+// Apple Fitness+
+// Apple News+
+// Apple Podcasts
+// Apple Books
+// App Store
+// Support
+// Apple TV+ Support
+// Apple Music Support
+
+// 4i
+// 
+// Shop All Accessories
+// Mac
+// iPad
+// iPhone
+// Apple Watch
+// AirPods
+// TV & Home
+// Explore Accessories
+// Made by Apple
+// Beats by Dr. Dre
+// AirTag
+
+// 4j
+// 
+// iPhone
+// Mac
+// iPad
+// Watch
+// AirPods
+// Music
+// TV
+// Explore Support
+// Get Help
+// Community
+// Check Coverage
+// Repair
+// Contact Us
+// Helpful Topics
+// Get AppleCare+
+// Apple ID & Password
+// Billing & Subscriptions
+// Find My
+// Accessibility
