d91392ec9770728bdb9c074d0f2d1c30477f68dd
kobra-soft <code.softwaredev@gmail.com>
2024-06-06 16:37:46 +0100

    NavBar - Fixes! (Submenu aligned with Container)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,61 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from "react";
 import { bagImg, searchImg } from "../utils";
 import { navLists } from "../constants";
-import './Navbar.css';
-import NavbarSubMenu from './NavbarSubMenu';
+import "./Navbar.css";
+import NavbarSubMenu from "./NavbarSubMenu";
 
 const Navbar = () => {
-  /* const navLists2 = ["Mac", "iPad", "iPhone", "Watch", "TV", "Music", "Support"]; */
+  const [isHovered, setIsHovered] = useState(false);
+  const firstNavItemRef = useRef(null);
 
+  useEffect(() => {
+    if (isHovered) {
+      const firstNavItem = firstNavItemRef.current;
+      const firstNavItemRect = firstNavItem.getBoundingClientRect();
+      const submenu = document.querySelector('.navbar-submenu');
+      submenu.style.left = `${firstNavItemRect.left}px`;
+    }
+  }, [isHovered]);
   return (
-    <header className="navbar">
-      <nav className="navbar-navigation">
-        <div className="navbar-logo">
-          {/* Insert your SVG for the Apple logo here */}
-        </div>
-
-        <div className="navbar-items text-[11.666px]">
-          {navLists.map((nav, index) => (
-            <div className="navbar-item text-[#a6a6a6] hover:text-[#E7E7E7] cursor-pointer" 
-            key={nav}>
-              {nav}
-              <div className="navbar-submenu">
-                {/* Insert your NavbarSubMenu component here */}
-                <NavbarSubMenu />
+    <div className="navbar-container bg-red-400">
+      <header className={`navbar ${isHovered ? "navbar-hovered" : ""}`}>
+        <nav className="navbar-navigation">
+          <div className="navbar-logo">
+            {/* Insert your SVG for the Apple logo here */}
+          </div>
+  
+          <div className="navbar-items text-[11.666px]">
+            {navLists.map((nav, index) => (
+              <div
+                className="navbar-item text-[#a6a6a6] hover:text-[#E7E7E7] cursor-pointer"
+                key={nav}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                ref={index === 0 ? firstNavItemRef : null}
+              >
+                {nav}
               </div>
-            </div>
-          ))}
-        </div>
-
-        <div className="navbar-icons">
-          {/* Insert your SVGs for the Search and Bag icons here */}
-        </div>
-      </nav>
-    </header>
+            ))}
+          </div>
+  
+          <div className="navbar-icons">
+            {/* Insert your SVGs for the Search and Bag icons here */}
+          </div>
+        </nav>
+      </header>
+      <div className="submenu-container">
+        {isHovered && (
+          <div className="navbar-submenu">
+            {/* Insert your NavbarSubMenu component here */}
+            <NavbarSubMenu
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            />
+          </div>
+        )}
+      </div>
+    </div>
   );
-};
+}
 
 export default Navbar;
