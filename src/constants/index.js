9d1f4a96f7d0db7546120dc0528d16d14e887f14
kobra-soft <code.softwaredev@gmail.com>
2024-06-14 23:55:48 +0100

    Commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,8 +116,10 @@ export const footerLinks = [
   { name: "Support", submenu: ["iPhone", "Mac", "iPad", "Watch", "AirPods", "Music", "TV"] },
 ]; */
 
+// 1-11
 export const navLists = [
   {
+    // 1
     name: "Store",
     title: "Shop",
     title2: "Quick Links",
@@ -128,14 +130,15 @@ export const navLists = [
       "iPad",
       "iPhone",
       "Apple Watch",
+      "Apple Vision Pro",
       "Accessories",
     ],
     submenu2: ["Find a Store", "Order Status", "Apple Trade In", "Financing"],
-    submenu3: ["Certified Refurbished",
-    "Education",
-    "Business"],
+    submenu3: ["Certified Refurbished", "Education", "Business"],
   },
+
   {
+    // 2
     name: "Mac",
     title: "Explore Mac",
     title2: "Shop Mac",
@@ -150,18 +153,25 @@ export const navLists = [
       "Mac Pro",
       "Displays",
     ],
-    submenu2: ["Find a Store", "Order Status", "Apple Trade In", "Financing"],
-    submenu3: ["Mac Support",
-    "AppleCare+ for Mac",
-    "macOS Sonoma",
-    "Apps by Apple",
-    "Continuity",
-    "iCloud+",
-    "Mac for Business",
-    "Education"],
+    submenu2: ["Shop Mac", "Mac Accessories", "Apple Trade In", "Financing"],
+    submenu3: [
+      "Mac Support",
+      "AppleCare+ for Mac",
+      "macOS Sonoma",
+      "Apps by Apple",
+      "Continuity",
+      "iCloud+",
+      "Mac for Business",
+      "Education",
+    ],
   },
+
   {
+    // 3
     name: "iPad",
+    title: "Explore iPad",
+    title2: "Shop iPad",
+    title3: "More from iPad",
     submenu: [
       "Explore All iPad",
       "iPad Pro",
@@ -171,9 +181,23 @@ export const navLists = [
       "Apple Pencil",
       "Keyboards",
     ],
+    submenu2: ["Shop iPad", "iPad Accessories", "Apple Trade In", "Financing"],
+    submenu3: [
+      "iPad Support",
+      "AppleCare+ for iPad",
+      "iPados 17",
+      "Apps by Apple",
+      "iCloud+",
+      "Education",
+    ],
   },
+
   {
+    // 4
     name: "iPhone",
+    title: "Explore iPhone",
+    title2: "Shop iPhone",
+    title3: "More from iPhone",
     submenu: [
       "Explore All iPhone",
       "iPhone 15 Pro",
@@ -182,66 +206,144 @@ export const navLists = [
       "iPhone 13",
       "iPhone SE",
     ],
+    submenu2: [
+      "Shop iPhone",
+      "iPhone Accessories",
+      "Apple Trade In",
+      "Financing",
+    ],
+    submenu3: [
+      "iPhone Support",
+      "AppleCare+ for iPhone",
+      "iOS 17",
+      "Apps by Apple",
+      "iPhone Privacy",
+      "iCloud+",
+      "Wallet, Pay",
+      "Siri",
+    ],
   },
+
   {
+    // 5
     name: "Watch",
-    submenu: [
-      "Explore All Apple Watch",
-      "Apple Watch Series 9",
-      "Apple Watch Ultra 2",
-      "Apple Watch SE",
-      "Apple Watch Nike",
-      "Apple Watch Hermès",
-    ],
+    title: "Explore Watch",
+    title2: "Shop Watch",
+    title3: "More From Watch",
+    submenu: ["Explore All Apple Watch",
+"Apple Watch Series 9",
+"Apple Watch Ultra 2",
+"Apple Watch SE",
+"Apple Watch Nike",
+"Apple Watch Hermès"],
+    submenu2: ["Shop Apple Watch",
+"Apple Watch Studio",
+"Apple Watch Straps",
+"Apple Watch Accessories",
+"Apple Trade In",
+"Financing"],
+    submenu3: ["Apple Watch Support",
+"AppleCare+",
+"watchOS 10",
+"Apps by Apple",
+"Apple Fitness+"],
   },
-  { name: "Vision", submenu: ["Explore All Apple Vision Pro"] },
+  // 6
+
   {
+    name: "Vision",
+    title: "Explore Vision",
+    title2: "Shop Vision",
+    title3: "NULL!!!",
+    submenu: ["Explore Apple Vision Pro"],
+    submenu2: ["Shop Apple Vision Pro",
+      "Apple Vision Pro Accessories"],
+    submenu3: ["NULL!!!"],
+  },
+
+  {
+    // 7
     name: "AirPods",
-    submenu: [
-      "Explore All AirPods",
-      "AirPods Pro (2nd generation)",
-      "AirPods (2nd generation)",
-      "AirPods (3rd generation)",
-      "AirPods Max",
-    ],
+    title: "Explore AirPods",
+    title2: "Shop AirPods",
+    title3: "More from AirPods",
+    submenu: ["Explore All AirPods",
+"AirPods Pro (2nd generation)",
+"AirPods (2nd generation)",
+"AirPods (3rd generation)",
+"AirPods Max"],
+    submenu2: ["Shop AirPods",
+"AirPods Accessories"],
+    submenu3: ["AirPods Support",
+"AppleCare+ for Headphones",
+"Apple Music"],
   },
+
   {
+    // 8
     name: "TV & Home",
-    submenu: ["Explore TV & Home", "Apple TV 4K", "HomePod", "HomePod mini"],
+    title: "Explore TV & Home",
+    title2: "Shop TV & Home",
+    title3: "More from TV & Home",
+    submenu: ["Explore TV & Home",
+"Apple TV 4K",
+"HomePod",
+"HomePod mini"],
+    submenu2: ["Shop Apple TV 4K",
+"Shop HomePod",
+"Shop HomePod mini",
+"Shop Siri Remote",
+"TV & Home Accessories"],
+    submenu3: ["Apple TV Support",
+"HomePod Support",
+"AppleCare+",
+"Apple TV app",
+"Apple TV+",
+"Home app",
+"Apple Music",
+"Siri",
+"AirPlay"],
   },
+
   {
+    // 9
     name: "Entertainment",
-    submenu: [
-      "Explore Entertainment",
-      "Apple One",
-      "Apple TV+",
-      "Apple Music",
-      "Apple Arcade",
-      "Apple Fitness+",
-      "Apple News+",
-      "Apple Podcasts",
-      "Apple Books",
-      "App Store",
-    ],
+    title: "",
+    title2: "",
+    title3: "",
+    submenu: [],
+    submenu2: [],
+    submenu3: [],
   },
+
   {
+    // 10
     name: "Accessories",
-    submenu: [
-      "Shop All Accessories",
-      "Mac",
-      "iPad",
-      "iPhone",
-      "Apple Watch",
-      "Apple Vision Pro",
-      "AirPods",
-      "TV & Home",
-    ],
+    title: "",
+    title2: "",
+    title3: "",
+    submenu: [],
+    submenu2: [],
+    submenu3: [],
   },
+
   {
+    // 11
     name: "Support",
-    submenu: ["iPhone", "Mac", "iPad", "Watch", "AirPods", "Music", "TV"],
+    title: "",
+    title2: "",
+    title3: "",
+    submenu: [],
+    submenu2: [],
+    submenu3: [],
   },
 ];
+
+
+
+
+
+
 
 export const navSubLinks_Search = [
   { name: "Quick Links", link: "/item1" },
