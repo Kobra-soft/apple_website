b07e0bacf7b8d3611f2dd0ea3e13a415b6a41792
kobra-soft <code.softwaredev@gmail.com>
2024-06-29 23:58:38 +0100

    Footer Links - Updated / Underlined / (Classnamed index.css)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,9 @@ const Footer = () => {
             the Financial Conduct Authority (Financial Services Register number:
             311753). Registered in England. Registered No: 2901725. Registered
             office: 1 Churchill Place, London, E14 5HP. Subject to application
-            and status. Full terms apply. <br className="mb-3"/>Barclays Partner Finance Eligibility
+            and status. Full terms apply. 
+            
+            <br className="mb-3"/>Barclays Partner Finance Eligibility
             Criteria apply. On the date of your application, you must be aged 18
             or over, have lived permanently in the UK for the last three years
             (i.e. provide three years of UK address history or up to the last
@@ -36,7 +38,9 @@ const Footer = () => {
             information, the credit check carried out will consider additional
             information relating to the performance of previous or existing
             commitments, and any county court judgments. Subject to application,
-            financial circumstances and borrowing history. <br className="mb-3"/>Customers who are
+            financial circumstances and borrowing history. 
+            
+            <br className="mb-3"/>Customers who are
             approved for finance will pay 0% (APR variable) for 24 months on a
             minimum order value of £99 including VAT. Maximum order value is
             £50,000 including VAT. To receive consumer finance on the Apple
@@ -46,7 +50,9 @@ const Footer = () => {
             example: £1,200 purchase. Deposit £0. Annual rate of interest 0%
             p.a. Representative APR 0%. Total amount of credit £1,200 paid over
             24 months as 24 monthly payments of £50 at 0% p.a. Total amount
-            payable £1,200. <br className="mb-3"/>* Trade-in values will vary based on the condition,
+            payable £1,200. 
+            
+            <br className="mb-3"/>* Trade-in values will vary based on the condition,
             year and configuration of your eligible trade-in device. Not all
             devices are eligible for credit. You must be at least 18 years old
             to be eligible to trade in for credit or for an Apple Gift Card.
@@ -59,8 +65,7 @@ const Footer = () => {
             is currently only available online and some stores may have
             additional requirements. Apple or its trade-in partners reserve the
             right to refuse or limit quantity of any trade-in transaction for
-            any reason. More details are available from Apple’s trade-in partner
-            for trade-in and recycling of eligible devices. Restrictions and
+            any reason. More details are available from <a className="link_underlined" See href="https://uk-appletradein.likewize.com/Login.aspx">Apple’s trade-in partner</a> for trade-in and recycling of eligible devices. Restrictions and
             limitations may apply.
             
             <br className="mb-2.5"/>1. Display size: The display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle. When measured as a standard rectangular shape, the screen is 6.12 inches (iPhone 15 Pro) or 6.69 inches (iPhone 15 Pro Max) diagonally. Actual viewable area is less.
@@ -72,11 +77,11 @@ const Footer = () => {
             <br className="mb-2.5"/>7. Ultra Wideband availability varies by region.
             <br className="mb-2.5"/>8. The included USB‑C Charge Cable is compatible with AirPods Pro (2nd generation) with MagSafe Charging Case (USB‑C).
             <br className="mb-2.5"/>9. Testing conducted by Apple in August 2023 using pre‑production iPhone 15, iPhone 15 Plus, iPhone 15 Pro and iPhone 15 Pro Max units and software and accessory Apple USB‑C Power Adapter (20W Model A2305). Fast-charge testing conducted with drained iPhone units. Charge time varies with settings and environmental factors; actual results will vary.
-            <br className="mb-2.5"/>10. Use of eSIM requires a wireless service plan (which may include restrictions on switching service providers and roaming, even after contract expires). Not all providers support eSIM. Use of eSIM in iPhone may be disabled when purchased from some providers. See your provider for details. To learn more, visit support.apple.com/en-gb/HT212780.
-            <br className="mb-2.5"/>11. Data plan required. 5G is available in selected markets and through selected network providers. Speeds vary based on site conditions and network. For details on 5G support, contact your provider and see apple.com/uk/iphone/cellular.
-            <br className="mb-2.5"/>12. All battery claims depend on network configuration and many other factors; actual results will vary. Battery has limited recharge cycles and may eventually need to be replaced. Battery life and charge cycles vary by use and settings. See apple.com/uk/batteries and apple.com/uk/iphone/battery.html for more information.
+            <br className="mb-2.5"/>10. Use of eSIM requires a wireless service plan (which may include restrictions on switching service providers and roaming, even after contract expires). Not all providers support eSIM. Use of eSIM in iPhone may be disabled when purchased from some providers. See your provider for details. To learn more, visit <a className="link_underlined" See href="https://support.apple.com/en-gb/118669">support.apple.com/en-gb/HT212780.</a>
+            <br className="mb-2.5"/>11. Data plan required. 5G is available in selected markets and through selected network providers. Speeds vary based on site conditions and network. For details on 5G support, contact your provider and see <a className="link_underlined" href="https://apple.com/uk/iphone/cellular">apple.com/uk/iphone/cellular.</a>            
+            <br className="mb-2.5"/>12. All battery claims depend on network configuration and many other factors; actual results will vary. Battery has limited recharge cycles and may eventually need to be replaced. Battery life and charge cycles vary by use and settings. See <a className="link_underlined" href="https://apple.com/uk/batteries">apple.com/uk/batteries</a> and <a className="link_underlined" href="https://apple.com/uk/iphone/battery.html">apple.com/uk/iphone/battery.html</a> for more information.            
             <br className="mb-2.5"/>13. Accessories sold separately.
-            <br className="mb-2.5"/>14. Service is included free for two years with the activation of any iPhone 15 model. Connection and response times vary based on location, site conditions and other factors. See support.apple.com/en-gb/HT213885 for more information.
+            <br className="mb-2.5"/>14. Service is included free for two years with the activation of any iPhone 15 model. Connection and response times vary based on location, site conditions and other factors.  <a className="link_underlined" See href="https://support.apple.com/en-gb/105097">support.apple.com/en-gb/HT213885</a> for more information.
             <br className="mb-2.5"/>15. iPhone 15 and iPhone 15 Pro can detect a serious car crash and call for help. Requires a mobile data connection or Wi‑Fi calling.
             <br className="mb-2.5"/>16. On a mass balance allocation.
             <br className="mb-2.5"/>17. Breakdown of US retail packaging by weight. Adhesives, inks and coatings are excluded from our calculations of plastic content and packaging weight.
@@ -84,7 +89,7 @@ const Footer = () => {
             <br className="mb-2.5"/>19. Personal Voice can be created using iPhone, iPad, and Mac with Apple silicon, and is available in English for all locales.
             <br className="mb-2.5"/>20. Door Detection and Point and Speak can read signs and labels in Cantonese (Simplified, Traditional), Chinese (Simplified, Traditional), English (US), French (France), German (Germany), Italian (Italy), Japanese (Japan), Korean (Republic of Korea), Portuguese (Brazil), Russian (Russia), Spanish (Spain) and Ukrainian (Ukraine). Detection Mode should not be relied on in circumstances where you may be harmed or injured, or in high-risk or emergency situations.
             <br className="mb-2.5"/>21. Requires an iPhone and Apple Watch with second-generation Ultra Wideband chip. Ultra Wideband availability varies by region.
-            <br className="mb-2.5"/><p className="">22. Battery life claim refers to larger models. All battery claims depend on network configuration and many other factors; actual results will vary. Battery has limited recharge cycles and may eventually need to be replaced. Battery life and charge cycles vary by use and settings. See <a className="underline text-[#e0e0e0]" href="https://apple.com/uk/batteries">apple.com/uk/batteries</a> and <a className="underline text-[#e0e0e0]" href="https://apple.com/uk/iphone/battery.html">apple.com/uk/iphone/battery.html</a> for more information.</p>
+            <br className="mb-2.5"/>22. Battery life claim refers to larger models. All battery claims depend on network configuration and many other factors; actual results will vary. Battery has limited recharge cycles and may eventually need to be replaced. Battery life and charge cycles vary by use and settings. See <a className="link_underlined" href="https://apple.com/uk/batteries">apple.com/uk/batteries</a> and <a className="link_underlined" href="https://apple.com/uk/iphone/battery.html">apple.com/uk/iphone/battery.html</a> for more information.
             <br className="mb-2.5"/>Apps are available on the App Store. Title availability is subject to change.
             <br className="mb-2.5"/>Apple TV+ requires a subscription.
           </p>
